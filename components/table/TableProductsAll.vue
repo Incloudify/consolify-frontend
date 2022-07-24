70ea5eb0676eb8ee3b2d772916e6827eb34e618f
Dispure <61584736+Dispure@users.noreply.github.com>
2022-07-24 20:57:47 +0800

    Improve ProductsInfo showing

--- components/table/TableProductsAll.vue
+++ components/table/TableProductsAll.vue
@@ -14,63 +14,18 @@
     <div
       class="text-h6"
     >
-      产品
+      保有产品
     </div>
     <!-- 产品标题 结束 -->
     <!-- 产品行 开始 -->
     <v-row>
-      <v-col>
-        <v-btn class="productbtn">
-          MCSM 面板服
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtn">
-          轻量应用服务器
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtn">
-          云服务器
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtn">
-          虚拟主机
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtn">
-          提瓦特桌面
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-btn class="productbtn">
-          甜甜花酿鸡
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtninvisit">
-          轻量应用服务器
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtninvisit">
-          云服务器
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtninvisit">
-          虚拟主机
-        </v-btn>
-      </v-col>
-      <v-col>
-        <v-btn class="productbtninvisit">
-          提瓦特桌面
-        </v-btn>
-      </v-col>
+      <div v-for="products in productsInfo.products" :key="products.key" class="products-div">
+        <v-col v-if="products.retention">
+          <v-btn>
+            {{ products.productName }} ({{ products.retentionCount }})
+          </v-btn>
+        </v-col>
+      </div>
     </v-row>
     <!-- 产品行 结束 -->
   </div>
@@ -80,7 +35,53 @@
 export default {
   data: () => ({
     tableFundCurrent: 0,
-    announcement: '近日行云因遭大流量攻击, 产品运行可能不稳定。'
+    announcement: '近日行云因遭大流量攻击, 产品运行可能不稳定。',
+    productsInfo: {
+      products: [
+        {
+          key: 1,
+          productName: '云服务器',
+          retention: true,
+          retentionCount: 2
+        },
+        {
+          key: 2,
+          productName: 'MCSM面板服',
+          retention: true,
+          retentionCount: 114
+        },
+        {
+          key: 3,
+          productName: '轻量应用服务器',
+          retention: false,
+          retentionCount: -1
+        },
+        {
+          key: 4,
+          productName: '虚拟主机',
+          retention: true,
+          retentionCount: 1
+        },
+        {
+          key: 5,
+          productName: '提瓦特桌面',
+          retention: false,
+          retentionCount: -1
+        },
+        {
+          key: 6,
+          productName: '甜甜花酿鸡',
+          retention: true,
+          retentionCount: 128
+        },
+        {
+          key: 7,
+          productName: '蒙德土豆饼',
+          retention: true,
+          retentionCount: 4
+        }
+      ]
+    }
   })
 }
 </script>
@@ -89,10 +90,10 @@ export default {
 .productbtn {
   width: 100%;
 }
-.productbtninvisit {
-  display: none;
-}
 .text-h6{
   margin-bottom: 15px;
+}
+.col{
+  padding-right: 2px;
 }
 </style>
