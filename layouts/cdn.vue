8560b524aadc27ebfcd1ee8ec56d110093904191
Someone-Yang <705043745@qq.com>
2022-07-26 23:30:54 +0800

    feat: 增加cdn页面

---
+++ layouts/cdn.vue
@@ -0,0 +1,65 @@
+<!-- mcs布局 -->
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col sm="2">
+            <v-list rounded>
+              <v-subheader>
+                内容分发网络
+              </v-subheader>
+              <v-list-item-group>
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>
+                      mdi-home
+                    </v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    概览
+                  </v-list-item-content>
+                </v-list-item>
+                <!---->
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>
+                      mdi-access-point
+                    </v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    域名列表
+                  </v-list-item-content>
+                </v-list-item>
+                <!---->
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>
+                      mdi-alert-octagram
+                    </v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    告警服务
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+          <v-col>
+            <Nuxt />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'CdnLayout',
+  theme: {
+    dark: false
+  }
+}
+</script>
---
+++ pages/cdn/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <p class="display-1 text--primary">
+      内容分发网络
+    </p>
+    <!-- 总览卡片 开始 -->
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <v-icon>
+              mdi-access-point-network
+            </v-icon>
+            已添加域名数
+          </v-card-title>
+          <v-card-text>
+            <div class="display-1">
+              114
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <v-icon>
+              mdi-access-point-check
+            </v-icon>
+            已开始分发数
+          </v-card-title>
+          <v-card-text>
+            <div class="display-1">
+              114
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <v-icon>
+              mdi-alarm-light
+            </v-icon>
+            告警中域名数
+          </v-card-title>
+          <v-card-text>
+            <div class="display-1" style="color: red;">
+              114
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- 总览卡片 结束 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CdnIndex',
+  layout: 'cdn',
+  data: () => ({
+  }),
+  head: () => ({
+    title: '概览 - 内容分发网络'
+  })
+}
+</script>
