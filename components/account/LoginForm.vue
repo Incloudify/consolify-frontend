f57adde267091c24a5ed03c7b431271c999ee7f2
Dispure <61584736+Dispure@users.noreply.github.com>
2022-09-04 21:23:00 +0800

    Add login page

---
+++ components/account/LoginForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-card
+      :loading="submitting === true"
+      elevation="0"
+      max-width="35%"
+      min-width="300px"
+    >
+      <br>
+      <v-text-field
+        label="Username"
+        hint="大小写A-z, 数字0-9, 下划线_"
+        outlined
+      />
+      <v-text-field
+        label="Password"
+        hint="注册时设置的密码"
+        :type="passwdShow ? 'text' : 'password'"
+        :append-icon="passwdShow ? 'mdi-eye' : 'mdi-eye-off'"
+        outlined
+        @click:append="passwdShow = !passwdShow"
+      />
+      <v-btn
+        rounded
+        block
+        elevation="1"
+        color="primary"
+        :loading="submitting"
+        @click="submitting = !submitting"
+      >
+        登录
+      </v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data: () => ({
+    submitting: false,
+    passwdShow: false
+  })
+}
+</script>
+
+<style>
+.v-card{
+  margin: 5px auto;
+  border-radius: 0 !important;
+}
+.v-btn{
+  margin: 0;
+}
+.v-input{
+  margin-top: 5px;
+}
+</style>
---
+++ pages/account/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <img src="~/static/incloudify-logo-large.png">
+    <h1>登录到玲行云端控制台</h1>
+    <AccountLoginForm />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'AccountLoginPage',
+  head: () => ({
+    title: '账户登录'
+  })
+}
+</script>
+<style>
+h1 {
+  font-weight: 200;
+  font-size: 25px;
+  margin-bottom: 15px;
+  margin-left: auto;
+  margin-right: auto;
+}
+img{
+  margin: 5px auto;
+  max-width: 150px;
+  max-height: 100px;
+}
+</style>
