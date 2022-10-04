d4d261389ffa1f3939a3ec75df7354c6f75ffea4
Dispure <61584736+Dispure@users.noreply.github.com>
2022-10-04 22:28:35 +0800

    [Improvement] 改进侧边栏的显示效果

--- layouts/mcs.vue
+++ layouts/mcs.vue
@@ -4,35 +4,33 @@
     <AppBar />
     <v-main>
       <v-row>
-        <v-col sm="1" lg="2">
-          <!-- 概览导航头部 开始 -->
-          <v-tabs class="list-tab" vertical optional>
-            <v-tab
-              to="/mcs/overview"
-            >
-              <v-icon>mdi-home</v-icon>
-              产品概览
-            </v-tab>
-            <v-tab
-              to="/mcs/productlist"
-            >
-              <v-icon>mdi-server</v-icon>
-              实例列表
-            </v-tab>
-            <v-tab
-              to="/mcs/cedlist"
-            >
-              <v-icon>mdi-harddisk</v-icon>
-              CED 列表
-            </v-tab>
-            <v-tab
-              to="/mcs/snapshotlist"
-            >
-              <v-icon>mdi-content-save</v-icon>
-              实例快照
-            </v-tab>
-          </v-tabs>
-        </v-col>
+        <!-- 概览导航头部 开始 -->
+        <v-tabs class="list-tab" vertical optional>
+          <v-tab
+            to="/mcs/overview"
+          >
+            <v-icon>mdi-home</v-icon>
+            产品概览
+          </v-tab>
+          <v-tab
+            to="/mcs/productlist"
+          >
+            <v-icon>mdi-server</v-icon>
+            实例列表
+          </v-tab>
+          <v-tab
+            to="/mcs/cedlist"
+          >
+            <v-icon>mdi-harddisk</v-icon>
+            CED 列表
+          </v-tab>
+          <v-tab
+            to="/mcs/snapshotlist"
+          >
+            <v-icon>mdi-content-save</v-icon>
+            实例快照
+          </v-tab>
+        </v-tabs>
         <v-col class="main-col">
           <Nuxt />
         </v-col>
@@ -55,5 +53,11 @@ export default {
 }
 .list-tab .v-icon{
   margin-right: 20px;
+}
+.list-tab{
+  margin-top: 10px;
+  margin-left: 12px;
+  min-width: 120px;
+  max-width: 220px;
 }
 </style>
