818f719175385e448c02fde8aa1cfd4289b5fceb
Dispure <61584736+Dispure@users.noreply.github.com>
2022-10-16 21:35:56 +0800

    [Feat] 增加针对访问首页时的SessionID校验

--- pages/table/index.vue
+++ pages/table/index.vue
@@ -13,6 +13,33 @@ export default {
   name: 'IndexPage',
   head: () => ({
     title: '概览 - 工作台'
-  })
+  }),
+  methods: {
+    getCookieValue (cookie, cookieName) {
+      for (let i = 0; i < cookie.length; i++) {
+        const splitedData = cookie[i].split('=')
+        if (splitedData[0] === cookieName) {
+          return splitedData[1]
+        }
+      }
+    },
+    validateSession () {
+      const cookie = document.cookie.split(';')
+      const sessionId = this.getCookieValue(cookie, 'sessionId')
+      // Send Cookie Value to the backend for validating
+      let validateResult = false
+      if (sessionId === '1AE2BD6716D871278F712A78E78C121120987D1283') {
+        validateResult = true
+      } else {
+        validateResult = false
+      }
+      if (!validateResult) {
+        window.location.replace('/account/login')
+      }
+    }
+  },
+  mounted () {
+    this.validateSession()
+  }
 }
 </script>
