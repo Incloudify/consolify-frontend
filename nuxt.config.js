e8fd0f352745c94ef809d58658130655af3e84d2
Dispure <61584736+Dispure@users.noreply.github.com>
2022-10-16 21:53:35 +0800

    [Chore] wssb

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
