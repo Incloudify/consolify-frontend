290de4905b900d1c0205dbea70baec5ed6fa79b3
Dispure <61584736+Dispure@users.noreply.github.com>
2022-10-17 21:59:32 +0800

    [Fix] 优化针对SessionID的校验流程(1/2)

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -9,10 +9,33 @@ export default {
     title: '账户'
   }),
   mounted () {
-    if (!document.cookie.includes('sessionId')) {
-      window.location.replace('/account/login')
-    } else {
-      window.location.replace('/account/settings')
+    this.ifSessionIdExist()
+  },
+  methods: {
+    getCookieValue (cookie, cookieName) {
+      for (let i = 0; i < cookie.length; i++) {
+        const splitedData = cookie[i].split('=')
+        if (splitedData[0] === cookieName) {
+          return splitedData[1]
+        }
+      }
+      return null
+    },
+    ifSessionIdExist () {
+      const cookie = document.cookie.split(';')
+      const sessionId = this.getCookieValue(cookie, 'sessionId')
+      if (sessionId !== null) {
+        let validateResult = false
+        this.$axios.post('http://127.0.0.1:4523/m1/1340156-0-0e7ed8c1/account/validateSession', '{"sessionId": "' + sessionId + '"}')
+          .then((data) => {
+            validateResult = data.data.code
+            if (validateResult === -1001) {
+              window.location.replace('/account/login')
+            }
+          })
+      } else if (sessionId === null) {
+        window.location.replace('/account/login')
+      }
     }
   }
 }
--- pages/table/index.vue
+++ pages/table/index.vue
@@ -25,18 +25,23 @@ export default {
           return splitedData[1]
         }
       }
+      return null
     },
     validateSession () {
       const cookie = document.cookie.split(';')
       const sessionId = this.getCookieValue(cookie, 'sessionId')
-      let validateResult = false
-      this.$axios.post('http://127.0.0.1:4523/m1/1340156-0-0e7ed8c1/account/validateSession', '{"sessionId": "' + sessionId + '"}')
-        .then((data) => {
-          validateResult = data.data.code
-          if (validateResult === -1001) {
-            window.location.replace('/account/login')
-          }
-        })
+      if (sessionId !== null) {
+        let validateResult = false
+        this.$axios.post('http://127.0.0.1:4523/m1/1340156-0-0e7ed8c1/account/validateSession', '{"sessionId": "' + sessionId + '"}')
+          .then((data) => {
+            validateResult = data.data.code
+            if (validateResult === -1001) {
+              window.location.replace('/account/login')
+            }
+          })
+      } else if (sessionId === null) {
+        window.location.replace('/account/login')
+      }
     }
   }
 }
